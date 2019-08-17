442a8d47e6f9788f36feaf8f71a4d0f1d1c7c98d
manakanemu <1274080117@qq.com>
2019-08-18 00:07:23 +0800

    增加修改标签大小功能

--- Reader.js
+++ Reader.js
@@ -41,63 +41,76 @@ function loadImg(i, first) {
     xhr.send()
 }
 
-if (!window.initReader) {
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
-    var container = document.getElementById('gdt')
-    var pageElements = container.getElementsByTagName('a')
-    window.pageUrls = new Array()
-    window.imgUrls = new Array()
-    window.imgElements = new Array()
-    window.progressBars = new Array()
-    window.nls = new Array()
-    window.loaded = 0
-    var readerContainer = document.createElement('div')
-    readerContainer.setAttribute('id', 'readerContainer')
-    container.parentElement.insertBefore(readerContainer, container)
-    var barBox = document.createElement('div')
-    barBox.setAttribute('id', 'barBox')
-    barBox.setAttribute('style', 'opacity:1;background-color: #7bed9f;z-index: 99; position: fixed; border-radius: 5px; border: none; width: 100%; height: 10px; left: 0px; top: 0px; display: flex; flex-flow: row nowrap; justify-content: start; align-items: center;')
-    readerContainer.appendChild(barBox)
+var importScript = document.getElementById('exReader')
+var tag_font_size = importScript.getAttribute('tag_font_size')
+if (tag_font_size) {
+    var style = document.createElement('style')
+    style.innerHTML = '.gt,.gtl,.gtw{font-size: '+tag_font_size+';}'
+    document.head.appendChild(style)
+}
 
-    for (var i = 0; i < pageElements.length; i++) {
-        window.pageUrls.push(pageElements[i].href)
-        var img = document.createElement('img')
-        img.setAttribute('id', 'img' + i)
-        img.setAttribute('name', 'anchor' + i)
-        img.setAttribute('order', i)
-        img.setAttribute('style', 'width:100%')
-        img.onload = imgLoadSuccess
-        img.onerror = imgLoadFailed
-        readerContainer.append(img)
-        window.imgElements[i] = img
-        var bar = document.createElement('a')
-        bar.setAttribute('href', '#anchor' + i)
-        bar.setAttribute('style', 'opacity:1;z-index: 100;display: flex; flex-grow: 1; background-color: #ff6b81;height: 100%;')
-        barBox.appendChild(bar)
-        window.progressBars[i] = bar
-    }
-    container.parentElement.removeChild(container)
-    window.initReader = true
-    for (var i = 0; i < window.pageUrls.length; i++) {
-        loadImg(i, true)
-    }
-} else {
-    window.loaded = 0
-    for (var i = 0; i < window.pageUrls.length; i++) {
-        if (!window.imgElements[i].complete) {
-            window.progressBars[i].style.backgroundColor = '#ffa502'
-            if (!window.imgUrls[i]) {
-                loadImg(i, false)
-            } else {
-                window.pageUrls[i] += '?nl=' + window.nls[i]
-                loadImg(i, false)
-            }
-        } else {
-            window.loaded++
+
+if (document.location.href.indexOf('https://exhentai.org/g/') > -1) {
+    if (!window.initReader) {
+        console.log('first')
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
+        var container = document.getElementById('gdt')
+        var pageElements = container.getElementsByTagName('a')
+        window.pageUrls = new Array()
+        window.imgUrls = new Array()
+        window.imgElements = new Array()
+        window.progressBars = new Array()
+        window.nls = new Array()
+        window.loaded = 0
+        var readerContainer = document.createElement('div')
+        readerContainer.setAttribute('id', 'readerContainer')
+        container.parentElement.insertBefore(readerContainer, container)
+        var barBox = document.createElement('div')
+        barBox.setAttribute('id', 'barBox')
+        barBox.setAttribute('style', 'opacity:1;background-color: #7bed9f;z-index: 99; position: fixed; border-radius: 5px; border: none; width: 100%; height: 10px; left: 0px; top: 0px; display: flex; flex-flow: row nowrap; justify-content: start; align-items: center;')
+        readerContainer.appendChild(barBox)
+
+        for (var i = 0; i < pageElements.length; i++) {
+            window.pageUrls.push(pageElements[i].href)
+            var img = document.createElement('img')
+            img.setAttribute('id', 'img' + i)
+            img.setAttribute('name', 'anchor' + i)
+            img.setAttribute('order', i)
+            img.setAttribute('style', 'width:100%')
+            img.onload = imgLoadSuccess
+            img.onerror = imgLoadFailed
+            readerContainer.append(img)
+            window.imgElements[i] = img
+            var bar = document.createElement('a')
+            bar.setAttribute('href', '#anchor' + i)
+            bar.setAttribute('style', 'opacity:1;z-index: 100;display: flex; flex-grow: 1; background-color: #ff6b81;height: 100%;')
+            barBox.appendChild(bar)
+            window.progressBars[i] = bar
         }
-    }
-    if (window.loaded >= window.pageUrls.length) {
-        hideElement(document.getElementById('barBox'))
+        container.parentElement.removeChild(container)
+        window.initReader = true
+        for (var i = 0; i < window.pageUrls.length; i++) {
+            loadImg(i, true)
+        }
+    } else {
+        console.log('seccond')
+        window.loaded = 0
+        for (var i = 0; i < window.pageUrls.length; i++) {
+            if (!window.imgElements[i].complete) {
+                window.progressBars[i].style.backgroundColor = '#ffa502'
+                if (!window.imgUrls[i]) {
+                    loadImg(i, false)
+                } else {
+                    window.pageUrls[i] += '?nl=' + window.nls[i]
+                    loadImg(i, false)
+                }
+            } else {
+                window.loaded++
+            }
+        }
+        if (window.loaded >= window.pageUrls.length) {
+            hideElement(document.getElementById('barBox'))
+        }
     }
 }
