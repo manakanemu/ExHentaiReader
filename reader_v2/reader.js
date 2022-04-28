cdf430db18505ab7d69c9bb0d3d008189fc29678
manakanemu <36415431+manakanemu@users.noreply.github.com>
2022-04-28 23:59:24 +0800

    Update reader.js

--- reader_v2/reader.js
+++ reader_v2/reader.js
@@ -14,21 +14,54 @@ class GalleryParser{
         this.gallery_subtitle = ''
         this.gallery_cover = ''
         this.gallery_page = ''
-
-        this.is_init = false
+        this.num_pictures = 0
+        this._is_init = false
+        this.init_waiting_stack = []
         this.read_gallery_info()
     }
+    get is_init(){
+        return this._is_init
+    }
+    set is_init(value){
+        this._is_init = value
+        for(let func of this.init_waiting_stack){
+            func()
+        }
+    }
     read_gallery_info(){
+        function get_right_info(key){
+            const e_info = document.getElementById('gdd')
+            const e_rows = e_info.getElementsByTagName('tr')
+            for(let e_row of e_rows){
+                const e_key = e_row.getElementsByClassName('gdt1')[0]
+                const e_value = e_row.getElementsByClassName('gdt2')[0]
+                const _key = e_key.innerText.trim().replace(':','')
+                const _value = e_value.innerText.trim()
+                if(key ===_key){
+                    return _value
+                }
+            }
+            return null
+        }
         info('read gellery info')
         const title = document.getElementById('gn') && document.getElementById('gn').innerText
-        const subtitle = document.getElementById('gj') && document.getElementById('gj').innerText || ''
         if(title){
-            this.gallery_title = title
-            this.gallery_subtitle = subtitle
-            const e_cover = document.getElementById('gd1')
-            const cover_url = e_cover.innerHTML.match(/url\((.*)\) no-repeat/i)[1]
-            this.gallery_cover = cover_url || ''
-            this.is_init = true
+            try{
+                const subtitle = document.getElementById('gj') && document.getElementById('gj').innerText || ''
+                const e_cover = document.getElementById('gd1')
+                const cover_url = e_cover.innerHTML.match(/url\((.*)\) no-repeat/i)[1]
+                const num_pictures = get_right_info('Length')
+
+                this.gallery_title = title
+                this.gallery_subtitle = subtitle
+                this.gallery_cover = cover_url
+                this.gallery_page = window.location.origin + window.location.pathname
+                this.num_pictures = Number(num_pictures.replace('pages',''))
+                
+                this.is_init = true
+            }catch(e){
+                error(e)
+            }
         }
         if(!this.is_init){
             requestAnimationFrame(this.read_gallery_info.bind(this))
