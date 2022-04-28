7c850ef64e97ff7199db61b2701c6aa941cdfaa1
manakanemu <36415431+manakanemu@users.noreply.github.com>
2022-04-29 00:42:24 +0800

    Update reader.js

--- reader_v2/reader.js
+++ reader_v2/reader.js
@@ -15,18 +15,24 @@ class GalleryParser{
         this.gallery_cover = ''
         this.gallery_page = ''
         this.num_pictures = 0
+        this.num_picture_per_page = 0
         this._is_init = false
         this.init_waiting_stack = []
-        this.read_gallery_info()
+        this.init()
     }
     get is_init(){
         return this._is_init
     }
     set is_init(value){
         this._is_init = value
-        for(let func of this.init_waiting_stack){
-            func()
+        if(value === true){
+            for(let func of this.init_waiting_stack){
+                func()
+            }
         }
+    }
+    init(){
+        this.read_gallery_info()
     }
     read_gallery_info(){
         function get_right_info(key){
@@ -51,11 +57,13 @@ class GalleryParser{
                 const e_cover = document.getElementById('gd1')
                 const cover_url = e_cover.innerHTML.match(/url\((.*)\) no-repeat/i)[1]
                 const num_pictures = get_right_info('Length')
+                const num_picture_per_page = document.getElementsByClassName('gdtm').length
 
                 this.gallery_title = title
                 this.gallery_subtitle = subtitle
                 this.gallery_cover = cover_url
                 this.gallery_page = window.location.origin + window.location.pathname
+                this.num_picture_per_page = num_picture_per_page
                 this.num_pictures = Number(num_pictures.replace('pages',''))
                 
                 this.is_init = true
@@ -68,4 +76,38 @@ class GalleryParser{
         }
     }
 }
+class PictureInfo{
+    constructor(){
+        
+        this.num_pictures = 0
+        this.
+        this._is_init = false
+        this.init_waiting_stack = []
+
+
+        if(gp.is_init){
+            this.init()
+        }else{
+            gp.init_waiting_stack.push(this.init.bind(this))
+        }
+    }
+    get is_init(){
+        return this._is_init
+    }
+    set is_init(value){
+        this._is_init = value
+        if(value === true){
+            for(let func of this.init_waiting_stack){
+                func()
+            }
+        }
+        
+    }
+
+    init(){
+        print('infoinit')
+    }
+}
 var gp = new GalleryParser()
+var pi = new PictureInfo()
+
